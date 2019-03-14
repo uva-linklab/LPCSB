692e9f92bf95867a566084a02eba6409940d892b
ags9xd <42878115+ags9xd@users.noreply.github.com>
2019-03-14 14:13:49 -0400

    Create index.js for Eddystone testing

---
+++ summon/LPCSB/index.js
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!-- HTML for Template Summon UI -->
+<html>
+    <head>
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' data: gap: https://ssl.gstatic.com * 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; img-src 'self' data: raw.githubusercontent.com; connect-src 'self' raw.githubusercontent.com">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="msapplication-tap-highlight" content="no">
+        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css">
+        <link rel="stylesheet" href="css/font-awesome-4.4.0/css/font-awesome.min.css">
+
+        <title>LPCSB</title>
+    </head>
+    <body id="main_view" data-role="main" class="frame" style="text-align:center; font-family:sans-serif-thin;color:#888">
+
+        <div data-role="header" data-theme="b">
+                </br>
+                <text align=center style="font-size: 250%">
+                    <span style="font-weight: bold; color:#0099FE">L</span>
+                    <span style="font-weight: bold; color:#0099DC">P</span>
+                    <span style="font-weight: bold; color:#0099BA">C</span>
+                    <span style="font-weight: bold; color:#009998">S</span>
+                    <span style="font-weight: bold; color:#009976">B</span>
+                </text></br>
+                <h1 style="font-size: 90%;" id="title">deviceId</h1>
+
+        </div>
+
+        <div style="margin-top:10px;">
+            <span align="center" id="data_update">Waiting for data...</span>
+        </div>
+
+        <table border="0" align=center style="width:90%; font-family:'HelveticaNeue-Light', sans-serif-light, Arial, sans-serif; margin-top:10px;">
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Broadcasting from Room Number:</p></td>
+                <td align=center class="charValueCell" id="Room Number">0</td>
+            </tr>
+
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Sensor ID:</p></td>
+                <td align=center class="charValueCell" id="Sensor ID">0</td>
+            </tr>
+           
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Red:</p></td>
+                <td align=center class="charValueCell" id="Sensor ID">0</td>
+            </tr>
+
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Green:</p></td>
+                <td align=center class="charValueCell" id="Sensor ID">0</td>
+            </tr>
+
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Blue:</p></td>
+                <td align=center class="charValueCell" id="Sensor ID">0</td>
+            </tr>
+
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Color Temp:</p></td>
+                <td align=center class="charValueCell" id="Sensor ID">0</td>
+            </tr>
+
+            <tr>
+                <td align=center class="charTitleCell"><p style="width:90%">Lux:</p></td>
+                <td align=center class="charValueCell" id="Sensor ID">0</td>
+            </tr>
+
+        </table>
+        </br>
+
+
+       <div data-display="overlay" data-swipe-close="true" data-dismissible="false" data-position="right" data-role="panel" id="logPanel">
+            <h2>Log</h2>
+            <p id="console" ></p>
+       </div>
+
+        <div data-role="popup" data-overlay-theme="b" id="about_page">
+            <p>LPCSB: Test Data Recorder</p>
+            <p>Find out more at <a href="https://github.com/lab11/blees">https://github.com/lab11/blees</a>.</p>
+        </div>
+
+       <div data-role="footer" data-theme="b" data-position="fixed">
+          <div data-role="navbar">
+            <ul>
+                <li><a href="#about_page" data-rel="popup" data-position-to="window"><i class="fa fa-info-circle fa-2x"></i></br>About</a></li>
+                <li><a href="#logPanel"><i name = "logbook" class="fa fa-book fa-2x"></i></br>Log</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="js/jquery.mobile-1.4.5.min.js"></script>
+        <script type="text/javascript" src="js/bluetooth.js"></script>
+        <script type="text/javascript" src="js/index.js"></script>
+    </body>
+</html>
