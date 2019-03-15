5a29a271fabd7d3f1a6d92c89b1430abcb424dd5
ags9xd <42878115+ags9xd@users.noreply.github.com>
2019-03-15 11:09:13 -0400

    Test

--- summon/LPCSB/parse.js
+++ summon/LPCSB/parse.js
@@ -1,42 +1,40 @@
-/* Parse LPCSB advertisements */
+/* Parse Herald advertisements */
 
 var parse_advertisement = function (advertisement, cb) {
 
-    if (advertisement.localName === 'LPCSB') {
+    if (advertisement.localName === 'herald') {
         if (advertisement.manufacturerData) {
-            // Need at least 3 bytes: Two for manufacturer ID and one for service ID.
+            // Need at least 3 bytes. Two for manufacturer identifier and
+            // one for the service ID.
             if (advertisement.manufacturerData.length >= 3) {
                 // Check that manufacturer ID and service byte are correct
                 var manufacturer_id = advertisement.manufacturerData.readUIntLE(0, 2);
                 var service_id = advertisement.manufacturerData.readUInt8(2);
-                if (manufacturer_id == 0x02E0 && service_id == 0x31) {
-                    // OK! This looks like an LPCSB packet
+                if (manufacturer_id == 0x02E0 && service_id == 0x21) {
+                    // OK! This looks like a Herald packet
                     // Length should be at least header + version + 1 character
                     if (advertisement.manufacturerData.length >= (3+1+1)) {
-                        var LPCSB = advertisement.manufacturerData.slice(3);
+                        var herald = advertisement.manufacturerData.slice(3);
 
-                        //Sensor ID is first: ID = 0x44, shows that the sensor is connected properly.
-                        var sensorID = LPCSB.readUInt8(0);
+                        // Version is the first thing.
+                        var version = herald.readUInt8(0);
 
-                        //Want Little Endian (I think, need to double check this)
-                        var clear=LPCSB.readUintLE(1,2);    //Clear Data
-			            var red=LPCSB.readUIntLE(3,2);      //Red Data
-			            var green=LPCSB.readUIntLE(5,2);    //Green Data
-                        var blue=LPCSB.readUIntLE(7,2);     //Blue Data
-                        var colorTemp=LPCSB.readUIntLE(9,2);//Color Temp Data
-                        var lux=LPCSB.readUIntLE(11,2);     //Lux Data
-                        var out = {
-                            device: 'LPCSB',
-                            sensorID: sensorID,
-			                clear: clear,
-                            red: red,
-                            green: green,
-                            blue: blue,
-                            colorTemp: colorTemp,
-                            lux: lux
-			                // _meta: {
-                            //     room: room
-                            // },
+                        //var room = herald.readUInt16BE(1).toString();
+                        //var room = herald.toString();
+                        //var room=hex2a(herald);
+                        var room=herald.toString('ascii',1,5);
+			var counter=herald.readUIntLE(5,4);
+			var seq_no=herald.readUIntLE(9,4);
+			var out = {
+                            device: 'Herald',
+                            
+                            room_string: room,
+                            version: version,
+			    counter: counter,
+			    sequence_no: seq_no,
+			    _meta: {
+                                room: room
+                            },
                         };
 
                         cb(out);
